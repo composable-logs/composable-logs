e9c5b43bd81085abf0dc24dc1efa2593c37b9f23
Matias Dahl <matias.dahl@iki.fi>
2021-08-07 19:26:02 +0300

    add MIT license

--- workspace/pynb_dag_runner/setup.py
+++ workspace/pynb_dag_runner/setup.py
@@ -4,6 +4,10 @@ setup(
     name="pynb_dag_runner",
     author="Matias Dahl",
     author_email="matias.dahl@iki.fi",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
     url="https://github.com/pynb-dag-runner/pynb-dag-runner",
     version="0.0.1",
     packages=find_packages(exclude=("tests",)),
