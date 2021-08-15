1fd5547c6923d092a670470c9bfaaad6fc0f7889
Matias Dahl <matias.dahl@iki.fi>
2021-08-15 15:26:29 +0300

    10 iterations to avoid memory-full errors (?)

--- workspace/pynb_dag_runner/conftest.py
+++ workspace/pynb_dag_runner/conftest.py
@@ -27,6 +27,6 @@ def repeat_in_stress_tests(f):
     if os.environ.get("RUN_ENVIRONMENT", "") == "stress-tests":
         repeat_count = 50
     else:
-        repeat_count = 100
+        repeat_count = 10
 
     return pytest.mark.parametrize("repeat_count", range(repeat_count))(f)
