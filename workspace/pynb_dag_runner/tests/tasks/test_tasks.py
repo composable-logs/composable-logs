3993e2585b1b6137ac1da64ce8df17742c9e538c
Matias Dahl <matias.dahl@iki.fi>
2021-08-28 18:30:57 +0300

    add test for dag and timing compatibility

--- workspace/pynb_dag_runner/tests/tasks/test_tasks.py
+++ workspace/pynb_dag_runner/tests/tasks/test_tasks.py
@@ -1,5 +1,6 @@
 import time, itertools
 from pathlib import Path
+from typing import List
 
 #
 from pynb_dag_runner.tasks.tasks import PythonTask, get_task_dependencies
@@ -16,8 +17,49 @@ from pynb_dag_runner.core.dag_runner import (
     TaskDependencies,
     run_tasks,
 )
+from pynb_dag_runner.wrappers.runlog import Runlog
 
 ## TODO: all the below tests should run multiple times for stress testing
+
+
+### ---- test order dependence for PythonTasks ----
+
+
+def assert_compatibility(runlog_results: List[Runlog], task_id_dependencies):
+    """
+    Test:
+     - generic invariances for runlog timings (Steps 1, 2)
+     - order constraints in dependency DAG are satisfied by output timings (Step 3)
+    """
+
+    # Step 1: all task-id:s in order dependencies must have at least one runlog
+    # entry. (The converse need not hold.)
+    task_ids_in_runlog: List[str] = [runlog["task_id"] for runlog in runlog_results]
+    task_ids_in_dependencies: List[str] = [d["from"] for d in task_id_dependencies] + [
+        d["to"] for d in task_id_dependencies
+    ]
+    assert set(task_ids_in_dependencies) <= set(task_ids_in_runlog)
+
+    # Step 2: A task retry should not start before previous attempt for running task
+    # has finished.
+    def get_runlogs(task_id):
+        return [runlog for runlog in runlog_results if runlog["task_id"] == task_id]
+
+    for task_id in task_ids_in_runlog:
+        task_id_retries = get_runlogs(task_id)
+        for idx, _ in enumerate(task_id_retries):
+            if idx > 0:
+                assert (
+                    task_id_retries[idx - 1]["out.timing.end_ts"]
+                    < task_id_retries[idx]["out.timing.start_ts"]
+                )
+
+    # Step 3: Runlog timings satisfy the same order constraints as in DAG run order
+    # dependencies.
+    for rule in task_id_dependencies:
+        ts0 = max([runlog["out.timing.end_ts"] for runlog in get_runlogs(rule["from"])])
+        ts1 = min([runlog["out.timing.start_ts"] for runlog in get_runlogs(rule["to"])])
+        assert ts0 < ts1
 
 
 ### ---- Tests for get_task_dependencies ----
@@ -42,6 +84,7 @@ def test_get_task_dependencies():
 
 
 def test_tasks_runlog_output(tmp_path: Path):
+    dependencies = TaskDependencies()
     runlog_results = flatten(
         run_tasks(
             [
@@ -49,7 +92,7 @@ def test_tasks_runlog_output(tmp_path: Path):
                     lambda _: 123, get_run_path=lambda _: tmp_path, task_id="t1"
                 ),
             ],
-            TaskDependencies(),
+            dependencies,
         )
     )
     assert len(runlog_results) == 1
@@ -76,15 +119,19 @@ def test_tasks_runlog_output(tmp_path: Path):
     # assert that runlog json has been written to disk
     assert runlog_results[0].as_dict() == read_json(tmp_path / "runlog.json")
 
+    assert_compatibility(runlog_results, get_task_dependencies(dependencies))
+
 
 def test_tasks_run_in_parallel():
+    dependencies = TaskDependencies()
+
     runlog_results = flatten(
         run_tasks(
             [
                 PythonTask(lambda _: time.sleep(1.0), task_id="t0"),
                 PythonTask(lambda _: time.sleep(1.0), task_id="t1"),
             ],
-            TaskDependencies(),
+            dependencies,
         )
     )
 
@@ -96,11 +143,14 @@ def test_tasks_run_in_parallel():
     ]
     assert range_intersect(range1, range2)
 
+    assert_compatibility(runlog_results, get_task_dependencies(dependencies))
+
 
 def test_parallel_tasks_are_queued_based_on_available_ray_worker_cpus():
     start_ts = time.time_ns()
 
-    runlogs = flatten(
+    dependencies = TaskDependencies()
+    runlog_results = flatten(
         run_tasks(
             [
                 PythonTask(lambda _: time.sleep(0.5), task_id="t0"),
@@ -108,7 +158,7 @@ def test_parallel_tasks_are_queued_based_on_available_ray_worker_cpus():
                 PythonTask(lambda _: time.sleep(0.5), task_id="t2"),
                 PythonTask(lambda _: time.sleep(0.5), task_id="t3"),
             ],
-            TaskDependencies(),
+            dependencies,
         )
     )
     end_ts = time.time_ns()
@@ -120,13 +170,15 @@ def test_parallel_tasks_are_queued_based_on_available_ray_worker_cpus():
 
     task_runtime_ranges = [
         range(runlog["out.timing.start_ts"], runlog["out.timing.end_ts"])
-        for runlog in runlogs
+        for runlog in runlog_results
     ]
 
     # Check 2: if tasks are run on 2 CPU:s the intersection of three runtime ranges
     # should always be empty.
     for r1, r2, r3 in itertools.combinations(task_runtime_ranges, 3):
         assert range_is_empty(range_intersection(r1, range_intersection(r2, r3)))
+
+    assert_compatibility(runlog_results, get_task_dependencies(dependencies))
 
 
 def test_retry_logic_in_python_function_task():
@@ -142,14 +194,15 @@ def test_retry_logic_in_python_function_task():
 
     t0 = PythonTask(f, task_id="t0", timeout_s=timeout_s, n_max_retries=3)
     t1 = PythonTask(lambda _: 42, task_id="t1")
+    dependencies = TaskDependencies(t0 >> t1)
 
-    result_runlog = flatten(run_tasks([t0, t1], TaskDependencies(t0 >> t1)))
+    runlog_results = flatten(run_tasks([t0, t1], dependencies))
 
     # All retries should have a distinct run-id:s
-    assert len(set([r["parameters.run.id"] for r in result_runlog])) == 4
+    assert len(set([r["parameters.run.id"] for r in runlog_results])) == 4
 
     # t0 should be run 3 times, t1 once
-    assert [r["task_id"] for r in result_runlog] == ["t0", "t0", "t0", "t1"]
+    assert [r["task_id"] for r in runlog_results] == ["t0", "t0", "t0", "t1"]
 
     # remove non-deterministic runlog keys, and assert remaining keys are as expected
     def del_keys(a_dict, keys_to_delete):
@@ -165,7 +218,7 @@ def test_retry_logic_in_python_function_task():
                 "out.timing.end_ts",
             ],
         )
-        for r in result_runlog
+        for r in runlog_results
     ]
 
     expected_det_runlog = [
@@ -208,3 +261,4 @@ def test_retry_logic_in_python_function_task():
     ]
 
     assert deterministic_runlog == expected_det_runlog
+    assert_compatibility(runlog_results, get_task_dependencies(dependencies))
