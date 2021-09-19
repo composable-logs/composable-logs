22c5d86a76c1847b8b1b0eb58c709ea040dabd12
Matias Dahl <matias.dahl@iki.fi>
2021-09-19 15:47:50 +0300

    refactor Gantt-visualizer to be more modular

--- tools/draw-runlog-visuals/runtimes-gantt-chart.py
+++ tools/draw-runlog-visuals/runtimes-gantt-chart.py
@@ -3,28 +3,31 @@ import glob, json, datetime
 from pathlib import Path
 from argparse import ArgumentParser
 
-#
 
-parser = ArgumentParser()
-parser.add_argument(
-    "--runlogs_root", type=str, help="root directory for pipeline runlogs",
-)
-parser.add_argument(
-    "--output", type=str, help="Output filepath for writing Mermaid input file"
-)
-args = parser.parse_args()
+def args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--runlogs_root",
+        required=True,
+        type=str,
+        help="root directory for pipeline runlogs",
+    )
+    parser.add_argument(
+        "--gantt_mmd_outputfile",
+        type=str,
+        required=False,
+        help="Output filepath for writing Mermaid input file",
+    )
+    args = parser.parse_args()
 
-print("*** Command line parameters ***")
-print("  - runlogs_root   :", args.runlogs_root)
-print("  - output         :", args.output)
+    return args
 
 
-def load_sorted_runlogs():
-    runlogs = [
+def load_runlogs():
+    return [
         json.loads(Path(f).read_text())
-        for f in glob.glob(f"{args.runlogs_root}/**/runlog.json", recursive=True)
+        for f in glob.glob(f"{args().runlogs_root}/**/runlog.json", recursive=True)
     ]
-    return sorted(runlogs, key=lambda runlog: runlog["out.timing.start_ts"])
 
 
 def runlog_taskname(runlog) -> str:
@@ -34,44 +37,62 @@ def runlog_taskname(runlog) -> str:
         raise Exception(f"Unknown task_type field in runlog={str(runlog)}")
 
 
-def runlog_duration_text(runlog) -> str:
-    seconds: float = runlog["out.timing.duration_ms"] / 1000
-    if seconds <= 60:
-        return f"{round(seconds, 2)}s"
-    else:
-        dt = datetime.timedelta(seconds=seconds)
-        return (
-            (str(dt).replace(":", "h ", 1).replace(":", "m ", 1)[:-4] + "s")
-            .replace("0h ", "")
-            .replace("00m ", "")
-        )
+def make_gantt_mermaid_file_content(runlogs):
+    def runlog_duration_text(runlog) -> str:
+        # runtime for a runlog is only used when drawing Gantt-charts
+        seconds: float = runlog["out.timing.duration_ms"] / 1000
+        if seconds <= 60:
+            return f"{round(seconds, 2)}s"
+        else:
+            dt = datetime.timedelta(seconds=seconds)
+            return (
+                (str(dt).replace(":", "h ", 1).replace(":", "m ", 1)[:-4] + "s")
+                .replace("0h ", "")
+                .replace("00m ", "")
+            )
 
+    output_lines = [
+        "gantt",
+        "    %% Mermaid input file for drawing Gantt chart of runlog runtimes",
+        "    %% See https://mermaid-js.github.io/mermaid/#/gantt",
+        "    %%",
+        "    axisFormat %H:%M",
+        "    %%",
+        "    %% Give timestamps as unix timestamps (ms)",
+        "    dateFormat x",
+        "    %%",
+    ]
 
-with open(args.output, "wt") as f:
-    f.write(
-        """gantt
-    %% Mermaid input file for drawing Gantt chart of runlog runtimes
-    %% See https://mermaid-js.github.io/mermaid/#/gantt
-    %%
-    axisFormat %H:%M
-    %% Give timestamps as unix timestamps (ms)
-    dateFormat x
-"""
-    )
-
-    for runlog in load_sorted_runlogs():
-        f.write(f"""    section {runlog_taskname(runlog)}\n""")
+    for runlog in sorted(runlogs, key=lambda runlog: runlog["out.timing.start_ts"]):
+        output_lines += [f"""    section {runlog_taskname(runlog)}"""]
 
         # render failed tasks in red
+        assert runlog["out.status"] in ["SUCCESS", "FAILURE"]
         if runlog["out.status"] == "FAILURE":
             modifier = "crit"
         else:
             modifier = ""
 
-        f.write(
-            f"""    {runlog_duration_text(runlog)} - {runlog["out.status"]} :{modifier}, """
-            f"""{runlog["out.timing.start_ts"] // 1000000}, """
-            f"""{runlog["out.timing.end_ts"] // 1000000} \n"""
-        )
+        output_lines += [
+            ", ".join(
+                [
+                    f"""    {runlog_duration_text(runlog)} - {runlog["out.status"]} :{modifier} """,
+                    f"""{runlog["out.timing.start_ts"] // 1000000} """,
+                    f"""{runlog["out.timing.end_ts"] // 1000000} """,
+                ]
+            )
+        ]
+
+    return "\n".join(output_lines)
+
+
+print(" - runlogs_root :", args().runlogs_root)
+
+if args().gantt_mmd_outputfile is not None:
+    print(" - writing Mermaid Gantt output file to :", args().gantt_mmd_outputfile)
+    Path(args().gantt_mmd_outputfile).write_text(
+        make_gantt_mermaid_file_content(load_runlogs())
+    )
+    print(f"    done!")
 
 print("Done")
