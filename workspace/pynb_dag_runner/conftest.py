a8a9e59c84787f69d7e810ed363d42322234322c
Matias Dahl <matias.dahl@iki.fi>
2021-10-16 15:21:52 +0300

    Add OpenTelemetry logging helpers + helpers for testing span output properties (#30)
    * optl dependencies + first setup
    
    * extracted timeout_guard into own wrapper
    
    * add 'get_span_id' + test case
    
    * SpanRecorder + native python span unit test
    
    * one helper function
    
    * added span-recorder + unit testing for recorded optl spans
    
    * revert ci workflow definition
    
    * revised span generation + span attribute test
    
    * clean spans before running tests

--- workspace/pynb_dag_runner/conftest.py
+++ workspace/pynb_dag_runner/conftest.py
@@ -1,11 +1,20 @@
-# location of this file indicates root for pytest
+import shutil
+
+# location of this file (conftest.py) indicates root for pytest
 import pytest, ray
 
 
 @pytest.fixture(scope="session", autouse=True)
 def init_ray_before_all_tests():
+    # Clean up any spans from previous runs
+    shutil.rmtree("/tmp/spans", ignore_errors=True)
+
     # - Init ray once before all tests.
     # - This way there is no need for ray.init commands in the in test python files.
     #   Hence ray do not need to start when VS Code is discovering tests (which seems
     #   to generate errors).
-    ray.init(num_cpus=2)
+    ray.init(
+        num_cpus=2,
+        # enable tracing and write traces to /tmp/spans/<pid>.txt in JSONL format
+        _tracing_startup_hook="ray.util.tracing.setup_local_tmp_tracing:setup_tracing",
+    )
