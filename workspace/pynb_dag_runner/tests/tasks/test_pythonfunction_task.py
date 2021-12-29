5d953c87c4a966a943d5dccd7371714c65071b64
Matias Dahl <matias.dahl@iki.fi>
2021-12-29 17:38:56 +0200

    test opentelemetry logs for stuck task

--- workspace/pynb_dag_runner/tests/tasks/test_pythonfunction_task.py
+++ workspace/pynb_dag_runner/tests/tasks/test_pythonfunction_task.py
@@ -192,6 +192,56 @@ def test__python_function_task__outputs_otel_logs__should_fail_as_parameter(
     validate_spans(get_test_spans())
 
 
+def test__python_function_task__otel_logs_for_stuck_task():
+    def get_test_spans():
+        with SpanRecorder() as rec:
+
+            def f(_):
+                time.sleep(1e6)
+
+            task: RemoteTaskP = task_from_python_function(
+                f, tags={"id": "stuck_function"}, timeout_s=1.0
+            )
+            outcome: TaskOutcome = run_and_await_tasks(
+                [task], task, timeout_s=10, arg="dummy value"
+            )
+
+            # check Task outcome
+            assert isinstance(outcome, TaskOutcome)
+            assert "timeout" in str(outcome.error)
+
+        return rec.spans
+
+    def validate_spans(spans: Spans):
+        assert len(spans.filter(["name"], "task-dependency")) == 0
+
+        top_task_span: SpanDict = one(spans.filter(["name"], "execute-task"))
+        assert read_key(top_task_span, ["attributes", "tags.id"]) == "stuck_function"
+        assert read_key(top_task_span, ["attributes", "tags.task_type"]) == "Python"
+
+        # --- check timeout-guard span ---
+        timeout_span: SpanDict = one(spans.filter(["name"], "timeout-guard"))
+        assert read_key(timeout_span, ["attributes", "timeout_s"]) == 1.0
+
+        assert timeout_span["status"] == {
+            "description": "Timeout",
+            "status_code": "ERROR",
+        }
+
+        # --- check call-python-function span ---
+        assert len(spans.filter(["name"], "call-python-function")) == 0
+
+        # check nesting of above spans
+        assert spans.contains_path(
+            top_task_span,
+            timeout_span,
+        )
+
+        # assert_compatibility(spans)
+
+    validate_spans(get_test_spans())
+
+
 def _get_time_range(spans: Spans, span_id: str):
     task_top_span = one(
         spans.filter(["name"], "invoke-task")
