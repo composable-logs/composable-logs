78e755d19fac0492b116eac565d4836449724a89
Matias Dahl <matias.dahl@iki.fi>
2022-01-07 14:48:39 +0200

    adjust fixtures for ray.init to save memory

--- workspace/pynb_dag_runner/conftest.py
+++ workspace/pynb_dag_runner/conftest.py
@@ -4,15 +4,13 @@ import gc, shutil
 import pytest, ray
 
 
-@pytest.fixture(scope="session", autouse=True)
-def init_ray_before_all_tests():
-    # Clean up any spans from previous runs
-    shutil.rmtree("/tmp/spans", ignore_errors=True)
-
-    # - Init ray once before all tests.
-    # - This way there is no need for ray.init commands in the in test python files.
-    #   Hence ray do not need to start when VS Code is discovering tests (which seems
-    #   to generate errors).
+def _ray_init():
+    """
+    - Init ray for running tests.
+    - This way there is no need for ray.init commands in the in test python files.
+      Hence ray do not need to start when VS Code is discovering tests (which seems
+      to generate errors).
+    """
     ray.init(
         num_cpus=2,
         # enable tracing and write traces to /tmp/spans/<pid>.txt in JSONL format
@@ -20,8 +18,26 @@ def init_ray_before_all_tests():
     )
 
 
-@pytest.fixture(autouse=True)
-def setup_and_teardown():
+@pytest.fixture(scope="session", autouse=True)
+def init_ray_before_all_tests():
+    # Clean up any spans from previous runs
+    shutil.rmtree("/tmp/spans", ignore_errors=True)
+
+    _ray_init()
+    yield
+    ray.shutdown()
+
+
+@pytest.fixture(scope="function", autouse=True)
+def func_wrapper():
     gc.collect()
-    yield  # test is run
+    yield
     gc.collect()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def module_wrapper():
+    ray.shutdown()
+    _ray_init()
+    yield
+    ray.shutdown()
