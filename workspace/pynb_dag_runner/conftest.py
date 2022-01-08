ab5c7bcb53c10a7b44343a29d1ced28abcbc2f7a
Matias Dahl <matias.dahl@iki.fi>
2022-01-08 12:48:59 +0200

    revert conftest to development branch

--- workspace/pynb_dag_runner/conftest.py
+++ workspace/pynb_dag_runner/conftest.py
@@ -1,22 +1,7 @@
-import gc, shutil
+import shutil
 
 # location of this file (conftest.py) indicates root for pytest
 import pytest, ray
-
-
-def _ray_init():
-    """
-    - Init ray for running tests.
-    - This way there is no need for ray.init commands in the in test python files.
-      Hence ray do not need to start when VS Code is discovering tests (which seems
-      to generate errors).
-    """
-    ray.init(
-        num_cpus=2,
-        ignore_reinit_error=True,
-        # enable tracing and write traces to /tmp/spans/<pid>.txt in JSONL format
-        _tracing_startup_hook="ray.util.tracing.setup_local_tmp_tracing:setup_tracing",
-    )
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -24,33 +9,12 @@ def init_ray_before_all_tests():
     # Clean up any spans from previous runs
     shutil.rmtree("/tmp/spans", ignore_errors=True)
 
-    _ray_init()
-    yield
-    ray.shutdown()
-
-
-# _NR_RAY = 0
-
-
-# @pytest.fixture(scope="function", autouse=True)
-# def func_wrapper():
-#     global _NR_RAY
-
-#     _NR_RAY += 1
-
-#     if _NR_RAY % 10 == 0:
-#         # reinit Ray on every 10th tests
-#         ray.shutdown()
-#         _ray_init()
-
-#     yield
-
-#     pass
-
-
-# @pytest.fixture(scope="module", autouse=True)
-# def module_wrapper():
-#     ray.shutdown()
-#     _ray_init()
-#     yield
-#     ray.shutdown()
+    # - Init ray once before all tests.
+    # - This way there is no need for ray.init commands in the in test python files.
+    #   Hence ray do not need to start when VS Code is discovering tests (which seems
+    #   to generate errors).
+    ray.init(
+        num_cpus=2,
+        # enable tracing and write traces to /tmp/spans/<pid>.txt in JSONL format
+        _tracing_startup_hook="ray.util.tracing.setup_local_tmp_tracing:setup_tracing",
+    )
