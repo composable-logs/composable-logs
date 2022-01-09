b1afc335474b1f2f3cb0da57c6da33508053931d
Matias Dahl <matias.dahl@iki.fi>
2022-01-09 19:18:12 +0200

    commit notebook_always_fail.py test notebook

---
+++ workspace/pynb_dag_runner/tests/tasks/jupytext_test_notebooks/notebook_always_fail.py
@@ -0,0 +1,9 @@
+# %%
+P = {}
+# %% tags=["parameters"]
+# ---- During automated runs parameters will be injected in this cell ---
+# %%
+# -----------------------------------------------------------------------
+# %%
+raise Exception("This notebook always fails")
+# %%
