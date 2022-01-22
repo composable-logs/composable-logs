5510b20697a9d21d31511a0027737d8e698dd22c
Matias Dahl <matias.dahl@iki.fi>
2022-01-22 13:33:28 +0200

    add new otel-parser-cli package to python wheel

---
+++ workspace/pynb_dag_runner/pynb_log_parser/cli.py
@@ -0,0 +1,6 @@
+from pynb_dag_runner.helpers import read_json
+
+print("-- pynb_dag_runner: log parser cli --")
+
+def entry_point():
+    print("-- entry_point called --")
--- workspace/pynb_dag_runner/setup.py
+++ workspace/pynb_dag_runner/setup.py
@@ -9,8 +9,13 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
     ],
+    entry_points={
+        "console_scripts": [
+            'pynb_log_parser = pynb_log_parser.cli:entry_point',
+        ],
+    },
     url="https://github.com/pynb-dag-runner/pynb-dag-runner",
     version="0.0.1",
     install_requires=(Path("/home/host_user/requirements.txt").read_text().split("\n")),
-    packages=find_packages(exclude=("tests",)),
+    packages=find_packages(exclude=["tests", "tests.*"]),
 )
