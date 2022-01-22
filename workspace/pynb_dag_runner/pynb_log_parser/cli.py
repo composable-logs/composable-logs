f8fd2badc8482689be5dce6982e5e854dcdc3ab6
Matias Dahl <matias.dahl@iki.fi>
2022-01-22 15:44:34 +0200

    write task dependencies

--- workspace/pynb_dag_runner/pynb_log_parser/cli.py
+++ workspace/pynb_dag_runner/pynb_log_parser/cli.py
@@ -2,7 +2,10 @@ from pathlib import Path
 from argparse import ArgumentParser
 
 #
-from pynb_dag_runner.helpers import read_json
+from pynb_dag_runner.helpers import read_json, write_json
+from pynb_dag_runner.opentelemetry_helpers import Spans, SpanId
+from pynb_dag_runner.opentelemetry_task_span_parser import extract_task_dependencies
+
 
 def args():
     parser = ArgumentParser()
@@ -21,8 +24,22 @@ def args():
     return parser.parse_args()
 
 
+def write_to_output_dir(spans: Spans, output_directory: Path):
+    print(" - Writing tasks in spans to output_directory", output_directory)
+    output_directory.mkdir(parents=True, exist_ok=True)
+
+    # write task dependencies
+    write_json(
+        output_directory / "task_dependencies.json",
+        list(extract_task_dependencies(spans)),
+    )
+
+
 def entry_point():
     print("-- pynb_dag_runner: log parser cli --")
 
-    spans = read_json(args().input_span_file)
-    print("nr of spans", len(spans))
+    spans: Spans = Spans(read_json(args().input_span_file))
+    print("nr of spans loaded", len(spans))
+
+    if args().output_directory is not None:
+        write_to_output_dir(spans, args().output_directory)
