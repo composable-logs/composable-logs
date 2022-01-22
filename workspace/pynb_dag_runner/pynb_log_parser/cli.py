c8f09ee75cfc478e5f8f2b514bbfd2eadffd5d8c
Matias Dahl <matias.dahl@iki.fi>
2022-01-22 15:10:10 +0200

    initial command line argument parser based on tools/draw-runlog-visuals/render-runlogs.py

--- workspace/pynb_dag_runner/pynb_log_parser/cli.py
+++ workspace/pynb_dag_runner/pynb_log_parser/cli.py
@@ -1,6 +1,28 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+#
 from pynb_dag_runner.helpers import read_json
 
-print("-- pynb_dag_runner: log parser cli --")
+def args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--input_span_file",
+        required=True,
+        type=Path,
+        help="JSON file with logged OpenTelemetry spans",
+    )
+    parser.add_argument(
+        "--output_directory",
+        required=False,
+        type=Path,
+        help="output directory for writing tasks and logged artefacts",
+    )
+    return parser.parse_args()
+
 
 def entry_point():
-    print("-- entry_point called --")
+    print("-- pynb_dag_runner: log parser cli --")
+
+    spans = read_json(args().input_span_file)
+    print("nr of spans", len(spans))
