d8c3d0324a92c321bca1cd74e085d169ec05343d
Matias Dahl <matias.dahl@iki.fi>
2022-01-29 13:06:23 +0200

    write run-json:s into output directory structure

--- workspace/pynb_dag_runner/pynb_log_parser/cli.py
+++ workspace/pynb_dag_runner/pynb_log_parser/cli.py
@@ -1,4 +1,4 @@
-from typing import Any, Tuple
+from typing import Any, Tuple, List, Iterable
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -10,38 +10,61 @@ from pynb_dag_runner.opentelemetry_task_span_parser import extract_task_dependen
 # --- span parser ---
 
 FlowDict = Any
-TaskIterator = Any
 TaskDict = Any
-RunIterator = Any
+RunDict = Any
+ArtefactDict = Any
 
 
-def _artefact_iterator(spans: Spans, run_top_span):
+def _artefact_iterator(spans: Spans, task_run_top_span) -> List[ArtefactDict]:
     return []
 
 
-def _run_iterator(spans: Spans, task_top_span):
-    return []
+def _run_iterator(
+    spans: Spans, task_top_span
+) -> Iterable[Tuple[RunDict, Iterable[ArtefactDict]]]:
+    for task_run_top_span in spans.bound_under(task_top_span).filter(
+        ["name"], "retry-call"
+    ):
+        run_dict = {
+            "run_span_id": task_run_top_span["context"]["span_id"],
+            "start_time": task_run_top_span["start_time"],
+            "end_time": task_run_top_span["end_time"],
+            "duration_s": get_duration_s(task_run_top_span),
+            "status": task_run_top_span["status"],
+            "run_attributes": (
+                spans.bound_inclusive(task_run_top_span)
+                #
+                .get_attributes(allowed_prefixes={"run."})
+            ),
+        }
+        yield run_dict, _artefact_iterator(spans, task_run_top_span)
+
+    return iter([])
 
 
-def _task_iterator(spans: Spans) -> Tuple[TaskDict, RunIterator]:
+def _task_iterator(spans: Spans) -> Iterable[Tuple[TaskDict, Iterable[RunDict]]]:
     for task_top_span in spans.filter(["name"], "execute-task"):
-        task_attributes = {
+        task_dict = {
             "task_span_id": task_top_span["context"]["span_id"],
             "start_time": task_top_span["start_time"],
             "end_time": task_top_span["end_time"],
             "duration_s": get_duration_s(task_top_span),
             "status": task_top_span["status"],
-            "task_attributes": (
+            "task_dict": (
                 spans.bound_inclusive(task_top_span)
                 #
                 .get_attributes(allowed_prefixes={"task."})
             ),
         }
 
-        yield task_attributes, _run_iterator(spans, task_top_span)
+        yield task_dict, _run_iterator(spans, task_top_span)
+
+    return iter([])
 
 
-def get_flow_iterators(spans: Spans) -> Tuple[FlowDict, TaskIterator]:
+def get_flow_iterators(
+    spans: Spans,
+) -> Tuple[FlowDict, Iterable[Tuple[TaskDict, Iterable[RunDict]]]]:
     flow_attributes = {
         "task_dependencies": list(extract_task_dependencies(spans)),
         "flow_attributes": spans.get_attributes(allowed_prefixes={"flow."}),
@@ -50,40 +73,55 @@ def get_flow_iterators(spans: Spans) -> Tuple[FlowDict, TaskIterator]:
     return flow_attributes, _task_iterator(spans)
 
 
-def write_to_output_dir(spans: Spans, output_directory: Path):
-    print(" - Writing tasks in spans to output_directory", output_directory)
-    output_directory.mkdir(parents=True, exist_ok=True)
+def _status_summary(span_dict) -> str:
+    if span_dict["status"]["status_code"] == "OK":
+        return "OK"
+    else:
+        return "FAILED"
 
-    flow_dict, task_it_it_it = get_flow_iterators(spans)
-    write_json(output_directory / "flow.json", flow_dict)
 
-    for task_dict, retry_it in task_it_it_it:
-        if task_dict["task_attributes"]["task.task_type"] == "jupytext":
-            success_indicator = (
-                "OK" if task_dict["status"]["status_code"] == "OK" else "FAILED"
+def write_to_output_dir(spans: Spans, output_basepath: Path):
+    print(" - Writing tasks in spans to output_basepath", output_basepath)
+    output_basepath.mkdir(parents=True, exist_ok=True)
+
+    flow_dict, task_it = get_flow_iterators(spans)
+    write_json(output_basepath / "flow.json", flow_dict)
+
+    for task_dict, task_retry_it in task_it:
+        if task_dict["task_dict"]["task.task_type"] == "jupytext":
+            task_subdir: str = "--".join(
+                [
+                    "jupytext-notebook-task",
+                    task_dict["task_dict"]["task.notebook"]
+                    .replace("/", "-")
+                    .replace(".", "-"),
+                    task_dict["task_span_id"],
+                    _status_summary(task_dict),
+                ]
             )
-            dirname = [
-                "jupytext-notebook-task",
-                task_dict["task_attributes"]["task.notebook"]
-                .replace("/", "-")
-                .replace(".", "-"),
-                task_dict["task_span_id"],
-                success_indicator,
-            ]
-            task_dir_name = output_directory / "--".join(dirname)
 
         else:
-            raise Exception("Unknown task type")
+            raise Exception(f"Unknown task type for {task_dict}")
 
-        task_filepath: Path = output_directory / task_dir_name
-        task_filepath.mkdir(parents=True, exist_ok=False)
-        write_json(task_filepath / "task.json", task_dict)
+        task_basepath: Path = output_basepath / task_subdir
+        task_basepath.mkdir(parents=True, exist_ok=True)
+        write_json(task_basepath / "task.json", task_dict)
 
-        for retry_attributes, artefacts_it in retry_it:
+        for task_run_dict, task_run_artefacts in task_retry_it:
             print("-- retry --")
-            print(retry_attributes)
+            print(task_run_dict)
 
-            for artefact in artefacts_it:
+            run_basepath: Path = task_basepath / "--".join(
+                [
+                    f"run={task_run_dict['run_attributes']['run.retry_nr']}",
+                    task_run_dict["run_span_id"],
+                    _status_summary(task_run_dict),
+                ]
+            )
+            run_basepath.mkdir(parents=True, exist_ok=True)
+            write_json(run_basepath / "run.json", task_run_dict)
+
+            for artefact in task_run_artefacts:
                 pass
 
 
@@ -91,7 +129,7 @@ def write_to_output_dir(spans: Spans, output_directory: Path):
 
 # Example usage:
 #
-# pynb_log_parser --input_span_file pynb_log_parser/opentelemetry-spans.json --output_directory pynb_log_parser/tmp
+# pynb_log_parser --input_span_file pynb_log_parser/opentelemetry-spans.json --output_basepath pynb_log_parser/tmp
 
 
 def args():
@@ -103,7 +141,7 @@ def args():
         help="JSON file with logged OpenTelemetry spans",
     )
     parser.add_argument(
-        "--output_directory",
+        "--output_basepath",
         required=False,
         type=Path,
         help="output directory for writing tasks and logged artefacts",
@@ -117,5 +155,5 @@ def entry_point():
     spans: Spans = Spans(read_json(args().input_span_file))
     print("nr of spans loaded", len(spans))
 
-    if args().output_directory is not None:
-        write_to_output_dir(spans, args().output_directory)
+    if args().output_basepath is not None:
+        write_to_output_dir(spans, args().output_basepath)
