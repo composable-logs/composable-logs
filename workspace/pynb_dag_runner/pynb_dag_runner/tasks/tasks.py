90344a91b10da320a50b326738fe326e8970e8a1
Matias Dahl <matias.dahl@iki.fi>
2022-02-05 17:10:10 +0200

    add context propagation when triggering notebook task

--- workspace/pynb_dag_runner/pynb_dag_runner/tasks/tasks.py
+++ workspace/pynb_dag_runner/pynb_dag_runner/tasks/tasks.py
@@ -1,12 +1,16 @@
+from typing import Mapping
 from pathlib import Path
 
 #
 import opentelemetry as otel
-from opentelemetry.trace import StatusCode, Status  # type: ignore
+from opentelemetry.trace.propagation.tracecontext import (
+    TraceContextTextMapPropagator,
+)
 
 #
 from pynb_dag_runner.core.dag_runner import task_from_python_function
 from pynb_dag_runner.opentelemetry_helpers import AttributesDict
+from pynb_dag_runner.tasks.task_opentelemetry_logging import _log_artefact
 
 #
 from pynb_dag_runner.notebooks_helpers import JupytextNotebook, JupyterIpynbNotebook
@@ -16,12 +20,19 @@ def prefix_keys(prefix: str, a_dict: AttributesDict) -> AttributesDict:
     return {f"{prefix}.{k}": v for k, v in a_dict.items()}
 
 
-def log_artefact(name, content):
-    tracer = otel.trace.get_tracer(__name__)  # type: ignore
-    with tracer.start_as_current_span("artefact") as span:
-        span.set_attribute("name", name)
-        span.set_attribute("content", content)
-        span.set_status(Status(StatusCode.OK))
+def _get_traceparent() -> str:
+    """
+    Get implicit OpenTelemetry trace-parent for implicit span for context propagation
+    """
+    carrier: Mapping[str, str] = {}
+    TraceContextTextMapPropagator().inject(carrier=carrier)
+
+    # check that context-carrier is of type {"traceparent": <some string>}
+    assert isinstance(carrier, dict)
+    assert carrier.keys() == {"traceparent"}
+    assert isinstance(carrier["traceparent"], str)
+
+    return carrier["traceparent"]
 
 
 def make_jupytext_task_ot(
@@ -32,8 +43,8 @@ def make_jupytext_task_ot(
     num_cpus: int = 1,
     parameters: AttributesDict = {},
 ):
-    # run-attributes sans-baggage which can only be determined at run time
-
+    # Determine task run-attributes (except baggage which can only be determined at
+    # run time).
     run_attributes: AttributesDict = {
         **parameters,
         "task.notebook": str(notebook.filepath),
@@ -48,7 +59,13 @@ def make_jupytext_task_ot(
         try:
             notebook.evaluate(
                 output=evaluated_notebook,
-                parameters={"P": {**run_attributes, **baggage}},
+                parameters={
+                    "P": {
+                        **run_attributes,
+                        **baggage,
+                        "_opentelemetry_traceparent": _get_traceparent(),
+                    }
+                },
             )
 
         except BaseException as e:
@@ -56,7 +73,7 @@ def make_jupytext_task_ot(
 
         finally:
             # this is not run if notebook is killed by timeout
-            log_artefact("notebook.ipynb", evaluated_notebook.filepath.read_text())
+            _log_artefact("notebook.ipynb", evaluated_notebook.filepath.read_text())
 
     return task_from_python_function(
         f=run_notebook,
