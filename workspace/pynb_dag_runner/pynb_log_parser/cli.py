4b705a0d9892f598f8561907ecb514109f5f5c17
Matias Dahl <matias.dahl@iki.fi>
2022-02-06 15:36:44 +0200

    add old parser for reference

--- workspace/pynb_dag_runner/pynb_log_parser/cli.py
+++ workspace/pynb_dag_runner/pynb_log_parser/cli.py
@@ -90,6 +90,68 @@ def write_to_output_dir(spans: Spans, out_basepath: Path):
                         f"Unknown encoding of artefect: {str(artefact_dict)[:2000]}"
                     )
 
+''''
+# content of old parser from repo ./tools/draw-runlog-visuals/render-runlogs.py
+
+def runlog_taskname(runlog) -> str:
+    if runlog["task_type"] == "PythonNotebookTask":
+        return f"""{runlog["notebook_path"]} (nb)"""
+    else:
+        raise Exception(f"Unknown task_type field in runlog={str(runlog)}")
+
+
+def make_gantt_mermaid_file_content(runlogs):
+    """
+    Draw Gantt chart from runtimes
+    """
+
+    def runlog_duration_text(runlog) -> str:
+        # runtime for a runlog is only used when drawing Gantt-charts
+        seconds: float = runlog["out.timing.duration_ms"] / 1000
+        if seconds <= 60:
+            return f"{round(seconds, 2)}s"
+        else:
+            dt = datetime.timedelta(seconds=seconds)
+            return (
+                (str(dt).replace(":", "h ", 1).replace(":", "m ", 1)[:-4] + "s")
+                .replace("0h ", "")
+                .replace("00m ", "")
+            )
+
+    output_lines = [
+        "gantt",
+        "    %% Mermaid input file for drawing Gantt chart of runlog runtimes",
+        "    %% See https://mermaid-js.github.io/mermaid/#/gantt",
+        "    %%",
+        "    axisFormat %H:%M",
+        "    %%",
+        "    %% Give timestamps as unix timestamps (ms)",
+        "    dateFormat x",
+        "    %%",
+    ]
+
+    for runlog in sorted(runlogs, key=lambda runlog: runlog["out.timing.start_ts"]):
+        output_lines += [f"""    section {runlog_taskname(runlog)}"""]
+
+        # render failed tasks in red
+        assert runlog["out.status"] in ["SUCCESS", "FAILURE"]
+        if runlog["out.status"] == "FAILURE":
+            modifier = "crit"
+        else:
+            modifier = ""
+
+        output_lines += [
+            ", ".join(
+                [
+                    f"""    {runlog_duration_text(runlog)} - {runlog["out.status"]} :{modifier} """,
+                    f"""{runlog["out.timing.start_ts"] // 1000000} """,
+                    f"""{runlog["out.timing.end_ts"] // 1000000} """,
+                ]
+            )
+        ]
+
+    return "\n".join(output_lines)
+'''
 
 def make_mermaid_gantt_inputfile(spans: Spans) -> str:
     """
