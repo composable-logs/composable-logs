f79e7850f7d3de62ac1d4c2ba15e944cffaa0b41
Matias Dahl <matias.dahl@iki.fi>
2022-02-19 21:18:34 +0200

    PydarLogger add support for logging Matplotlib figures (#84)
    * test log spans from PydarLogger; add log_figure
    
    * include artefacts w subdiretory; comments
    
    * ensure filepath exists when writing binary files

--- workspace/pynb_dag_runner/tests/tasks/jupytext_test_notebooks/notebook_otel_logging.py
+++ workspace/pynb_dag_runner/tests/tasks/jupytext_test_notebooks/notebook_otel_logging.py
@@ -23,7 +23,7 @@ logger = PydarLogger(P)
 # test logging of artefact with utf-8 content
 
 logger.log_artefact("README.md", "foobar123")
-logger.log_artefact("binary.bin", bytes(range(256)))
+logger.log_artefact("class_a/binary.bin", bytes(range(256)))
 
 # %%
 # test logging of general json-serializable values
