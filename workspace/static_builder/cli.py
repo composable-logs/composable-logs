668aae7b60c5bb7eccc24447d8b90dfabe44f917
Matias Dahl <matias.dahl@iki.fi>
2022-03-13 21:14:03 +0200

    add cli-feature to download pipeline-run artefacts from selected Github repo (#4)
    * add ghapi python dependency
    
    * list all artifacts in github repo
    
    * add py requests library as dependency
    
    * add function to download artifact from github
    
    * process/iterator over task/runs in zip file and output (new) directory structure
    
    * connect cli

--- workspace/static_builder/cli.py
+++ workspace/static_builder/cli.py
@@ -1,2 +1,55 @@
+from typing import Any
+from pathlib import Path
+from argparse import ArgumentParser
+
+from .github_helpers import list_artifacts_for_repo, download_artifact
+from .static_builder import process_artifact
+
+# `static_builder` command line utility --- run `statuc_builder --help` for usage
+
+
+def args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--gh_repo_owner",
+        required=True,
+        type=str,
+        help="Github repository owner (organization or user name)",
+    )
+    parser.add_argument(
+        "--gh_repo_name",
+        required=True,
+        type=str,
+        help="Github repo name with artefacts to download",
+    )
+    parser.add_argument(
+        "--output_dir",
+        required=True,
+        type=str,
+        help="Output directory",
+    )
+    return parser.parse_args()
+
+
 def entry_point():
-    print("cli entrypoint")
+    gh_repo_owner = args().gh_repo_owner
+    gh_repo_name = args().gh_repo_name
+    output_dir = args().output_dir
+
+    print("gh_repo_owner   :", gh_repo_owner)
+    print("gh_repo_name    :", gh_repo_name)
+    print("output_dir      :", output_dir)
+
+    results = list_artifacts_for_repo(repo_owner=gh_repo_owner, repo_name=gh_repo_name)
+    for idx, entry in enumerate(results):
+        if entry["expired"]:
+            continue
+        print(idx, entry)
+
+        artefact_zip = download_artifact(
+            repo_owner=gh_repo_owner,
+            repo_name=gh_repo_name,
+            artifact_id=entry["id"],
+        )
+
+        process_artifact(artefact_zip, output_dir=Path(output_dir))
---
+++ workspace/static_builder/github_helpers.py
@@ -0,0 +1,83 @@
+import os
+from typing import Dict, List, Iterable
+
+#
+
+# For full list of ghapi methods, see https://ghapi.fast.ai/fullapi.html
+from ghapi.all import GhApi  # type: ignore
+import requests  # type: ignore
+
+
+def _paginator(operation, per_page=30, **kwargs) -> Iterable[Dict]:
+    """
+    Paginator-wrapper suitable for getting all results from list_artifacts_for_repo api.
+
+    The GhApi library has built in pagainator-wrapper (from ghapi.page import paged).
+    This is described here: https://ghapi.fast.ai/page.html
+
+    However, this wrapper seems to loop over a fixed number of pages (default 9999)
+    even when there is less data. This issue is known, see
+    https://github.com/fastai/ghapi/issues/96  which also includes a workaround.
+    However, this does not seem to work for list_artifacts_for_repo since
+    "incomplete_results" is not a key in results returned from this api.
+    """
+    count = 0
+    for page in range(1, 9999):
+        result = operation(**kwargs, per_page=per_page, page=page)
+        # assert "incomplete_results" not in result
+
+        if len(result["artifacts"]) == 0:
+            break
+        for entry in result["artifacts"]:
+            yield dict(entry)
+            count += 1
+
+    assert count == result["total_count"]
+
+
+def list_artifacts_for_repo(repo_owner: str, repo_name: str) -> List[Dict]:
+    """
+    List all artefacts in a Github repo.
+
+    Environment variable GITHUB_TOKEN should contain valid token (either token
+    generated for an action run, or a Github personal access token).
+
+    The required scope for the token is documented here:
+    https://docs.github.com/en/rest/reference/actions#artifacts
+
+    See above link for strucuture of return values.
+    """
+    api = GhApi()
+
+    return list(
+        _paginator(
+            api.actions.list_artifacts_for_repo, owner=repo_owner, repo=repo_name
+        )
+    )
+
+
+def download_artifact(repo_owner: str, repo_name: str, artifact_id: int) -> bytes:
+    """
+    Download artifact from Github repo
+
+    API Documentation
+    https://docs.github.com/en/rest/reference/actions#download-an-artifact
+
+    Note:
+     - download artifact api did not seem to work with GhApi library
+    """
+    token = os.getenv("GITHUB_TOKEN")
+    if token is None:
+        raise Exception("GITHUB_TOKEN should be set")
+
+    endpoint = "https://api.github.com/repos/"
+    url = f"{repo_owner}/{repo_name}/actions/artifacts/{artifact_id}/zip"
+    response = requests.get(
+        endpoint + url,
+        headers={"authorization": f"Bearer {token}"},
+        allow_redirects=True,
+    )
+    if response.status_code != requests.codes.ok:
+        raise Exception(f"Request failed with code {response.status_code}")
+
+    return response.content
