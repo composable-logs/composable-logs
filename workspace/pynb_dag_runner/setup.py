e4299e14bcca7870b4f20f1851c2b4d45b5aa044
Matias Dahl <matias.dahl@iki.fi>
2022-10-08 18:32:10 +0300

    preliminary move extra setup.py -> comments

--- workspace/pynb_dag_runner/setup.py
+++ workspace/pynb_dag_runner/setup.py
@@ -78,3 +78,25 @@ setup(
     install_requires=(Path("/home/host_user/requirements.txt").read_text().split("\n")),
     packages=find_packages(exclude=["tests", "tests.*"]),
 )
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="otel_output_parser",
+#     author="Matias Dahl",
+#     author_email="matias.dahl@iki.fi",
+#     license="MIT",
+#     classifiers=[
+#         "License :: OSI Approved :: MIT License",
+#     ],
+#     entry_points={
+#         "console_scripts": [
+#             "static_builder = static_builder.cli:entry_point",
+#         ],
+#     },
+#     url="https://github.com/pynb-dag-runner",
+#     version="0.0.1",
+#     install_requires=[],
+#     packages=find_packages(exclude=["tests", "tests.*"]),
+# )
