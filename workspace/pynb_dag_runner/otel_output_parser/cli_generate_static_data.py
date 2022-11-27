6ae8a050da723701ad38ef55c05b780df55c9173
Matias Dahl <matias.dahl@iki.fi>
2022-11-27 20:08:07 +0200

    add ui_static_data.json; fix directory structure

--- workspace/pynb_dag_runner/otel_output_parser/cli_generate_static_data.py
+++ workspace/pynb_dag_runner/otel_output_parser/cli_generate_static_data.py
@@ -16,7 +16,7 @@ from .mermaid_graphs import (
     make_mermaid_dag_inputfile,
     make_mermaid_gantt_inputfile,
 )
-from .static_builder.cli import write_attachment_sink
+from .static_builder.cli import write_attachment_sink, StaticMLFlowDataSink
 
 # -
 from .common_helpers.github_helpers import github_repo_artifact_zips
@@ -39,7 +39,7 @@ def args():
     )
     parser.add_argument(
         "--output_www_root_directory",
-        required=False,
+        required=True,
         type=Path,
         help="Output www-root directory (to write metadata json and artifacts)",
     )
@@ -163,12 +163,23 @@ def entry_point():
     print("zip_cache_dir              :", args().zip_cache_dir)
     print("output_www_root_directory  :", args().output_www_root_directory)
 
+    # if output_static_data_json is None, this sink is no-op
+    static_mlflow_data_sink = StaticMLFlowDataSink(
+        args().output_www_root_directory / "ui_static_data.json"
+    )
+
     for artifact_zip in github_repo_artifact_zips(
         github_repository=args().github_repository,
         zip_cache_dir=args().zip_cache_dir,
     ):
         spans = get_recorded_spans_from_zip(artifact_zip)
         for span_summary in linearize_log_events(spans):
-            write_attachment_sink(args().output_www_root_directory, span_summary)
+            write_attachment_sink(
+                args().output_www_root_directory / "pipeline-artifacts",
+                span_summary,
+            )
+            static_mlflow_data_sink.push(span_summary)
+
+    static_mlflow_data_sink.close()
 
     print("Done")
