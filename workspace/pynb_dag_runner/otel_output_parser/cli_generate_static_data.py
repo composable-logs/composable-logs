e5e6d455f3796eb65d1338e477ccd3f560efc2cc
Matias Dahl <matias.dahl@iki.fi>
2022-11-27 16:31:34 +0200

    open and parse zip files

--- workspace/pynb_dag_runner/otel_output_parser/cli_generate_static_data.py
+++ workspace/pynb_dag_runner/otel_output_parser/cli_generate_static_data.py
@@ -1,6 +1,15 @@
+import io, json
+from zipfile import ZipFile
+
 from pathlib import Path
 from functools import lru_cache
 from argparse import ArgumentParser
+
+# -
+from pynb_dag_runner.opentelemetry_helpers import Spans
+
+# -
+from .common_helpers.github_helpers import github_repo_artifact_zips
 
 
 @lru_cache
@@ -27,10 +36,34 @@ def args():
     return parser.parse_args()
 
 
+def get_recorded_spans_from_zip(zip_file_content: bytes) -> Spans:
+    """
+    Input:
+       zip_file_content: bytes
+            byte-array with zip file created by pipeline run process with recorded
+            OpenTelemetry spans recorded in a "opentelemetry-spans.json" file.
+
+    Output:
+       Spans object with parsed JSON
+
+    """
+    # convert byte-array into something that can be opened as a file
+    z_io: io.BytesIO = io.BytesIO(zip_file_content)
+
+    with ZipFile(z_io, "r") as z:  # type: ignore
+        return Spans(json.loads(z.read("opentelemetry-spans.json")))
+
+
 def entry_point():
     print("--- generate_static_data ---")
     print("github_repository          :", args().github_repository)
     print("zip_cache_dir              :", args().zip_cache_dir)
     print("output_www_root_directory  :", args().output_www_root_directory)
+
+    for artifact_zip in github_repo_artifact_zips(
+        github_repository=args().github_repository,
+        zip_cache_dir=args().zip_cache_dir,
+    ):
+        print(get_recorded_spans_from_zip(artifact_zip))
 
     print("Done")
