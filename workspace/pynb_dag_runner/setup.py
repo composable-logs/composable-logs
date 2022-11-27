5cd7885260dcbcebf0bc9f63fbbbe47d6983bf8b
Matias Dahl <matias.dahl@iki.fi>
2022-11-27 13:23:56 +0200

    rename old cli.py

--- workspace/pynb_dag_runner/setup.py
+++ workspace/pynb_dag_runner/setup.py
@@ -95,7 +95,7 @@ setup(
     entry_points={
         "console_scripts": [
             "static_builder = otel_output_parser.static_builder.cli:entry_point",
-            "pynb_log_parser = otel_output_parser.cli:entry_point",
+            "pynb_log_parser = otel_output_parser.cli_pynb_log_parser:entry_point",
         ],
     },
     url="https://github.com/pynb-dag-runner/pynb-dag-runner",
