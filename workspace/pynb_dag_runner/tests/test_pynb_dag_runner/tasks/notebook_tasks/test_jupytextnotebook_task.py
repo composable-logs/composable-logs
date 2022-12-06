7790278c6e66f305d49dfccba7d41274df992d42
Matias Dahl <matias.dahl@iki.fi>
2022-12-06 13:23:58 +0200

    delete test with notebook failing conditionally on retry nr

--- workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/notebook_tasks/test_jupytextnotebook_task.py
+++ workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/notebook_tasks/test_jupytextnotebook_task.py
@@ -44,7 +44,6 @@ TEST_NOTEBOOK_PATH = (Path(__file__).parent) / "jupytext_test_notebooks"
 def make_test_nb_task(
     nb_name: str, max_nr_retries: int, parameters={}, timeout_s: float = 10.0
 ):
-    # nb_path: Path = (Path(__file__).parent) / "jupytext_test_notebooks"
     return make_jupytext_task_ot(
         notebook=JupytextNotebook(TEST_NOTEBOOK_PATH / nb_name),
         tmp_dir=TEST_NOTEBOOK_PATH,
@@ -241,94 +240,6 @@ def test__jupytext_notebook_task__always_fail():
             spans.contains_path(
                 top_task_span, top_retry_span, retry_span, artefact_span
             )
-
-    validate_spans(get_test_spans())
-
-
-@pytest.mark.parametrize("N_retries", [2, 10])
-def test__jupytext_notebook_task__exception_throwing_notebook(N_retries):
-    def get_test_spans():
-        with SpanRecorder() as rec:
-            jupytext_task = make_test_nb_task(
-                nb_name="notebook_exception.py",
-                max_nr_retries=N_retries,
-                parameters={"task.variable_a": "task-value"},
-            )
-            _ = start_and_await_tasks([jupytext_task], [jupytext_task], arg={})
-
-        return rec.spans
-
-    # notebook will fail on first three runs. Depending on number of retries
-    # determine which run:s are success/failed.
-    def ok_indices():
-        if N_retries == 2:
-            return []
-        else:
-            return [3]
-
-    def failed_indices():
-        if N_retries == 2:
-            return [0, 1]
-        else:
-            return [0, 1, 2]
-
-    def nr_notebook_executions():
-        if N_retries == 2:
-            return 2
-        else:
-            return 4
-
-    def validate_spans(spans: Spans):
-        top_task_span = one(
-            spans.filter(["name"], "execute-task")
-            #
-            .filter(["attributes", "task.task_type"], "jupytext")
-        )
-
-        top_retry_span = one(spans.filter(["name"], "retry-wrapper"))
-        assert spans.contains_path(top_task_span, top_retry_span)
-        assert (
-            read_key(top_retry_span, ["attributes", "task.max_nr_retries"]) == N_retries
-        )
-
-        retry_call_spans = spans.filter(["name"], "retry-call")
-        assert len(retry_call_spans) == nr_notebook_executions()
-
-        if len(ok_indices()) > 0:
-            assert top_task_span["status"] == {"status_code": "OK"}
-        else:
-            assert top_task_span["status"] == {
-                "status_code": "ERROR",
-                "description": "Remote function call failed",
-            }
-        run_spans = spans.filter(["name"], "retry-call").sort_by_start_time()
-        assert len(run_spans) == len(ok_indices()) + len(failed_indices())
-
-        for idx in ok_indices():
-            assert run_spans[idx]["status"] == {"status_code": "OK"}
-
-        for idx in failed_indices():
-            failed_run_span = run_spans[idx]
-
-            exception = one(spans.bound_inclusive(failed_run_span).exception_events())[
-                "attributes"
-            ]
-            assert exception["exception.type"] == "Exception"
-            assert "Thrown from notebook!" in exception["exception.message"]
-
-            assert run_spans[idx]["status"] == {
-                "status_code": "ERROR",
-                "description": "Run failed",
-            }
-
-        for idx in failed_indices() + ok_indices():
-            # for both successful and failed runs, a partially evaluated notebook
-            # should have been logged as an artefact.
-            artefact_span = one(
-                spans.bound_under(run_spans[idx]).filter(["name"], "artefact")
-            )
-            assert artefact_span["attributes"]["name"] == "notebook.ipynb"
-            assert str(1 + 12 + 123) in artefact_span["attributes"]["content_encoded"]
 
     validate_spans(get_test_spans())
 
