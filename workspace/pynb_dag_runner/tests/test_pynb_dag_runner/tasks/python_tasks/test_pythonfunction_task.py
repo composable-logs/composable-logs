bf12cbffe3f88c9b982e7dbe1efbf6aa489d233b
Matias Dahl <matias.dahl@iki.fi>
2022-12-06 18:05:02 +0200

    own file for test__python_function_task__otel_logs_for_stuck_task

--- workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_pythonfunction_task.py
+++ workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_pythonfunction_task.py
@@ -39,56 +39,6 @@ from .py_test_helpers import get_time_range
 ### ---- Test Python task evaluation ----
 
 
-def test__python_function_task__otel_logs_for_stuck_task():
-    def get_test_spans():
-        with SpanRecorder() as rec:
-
-            def f(_):
-                time.sleep(1e6)
-
-            task: RemoteTaskP = task_from_python_function(
-                f, attributes={"task.id": "stuck_function"}, timeout_s=1.0
-            )
-            [outcome] = start_and_await_tasks(
-                [task], [task], timeout_s=10, arg="dummy value"
-            )
-
-            # check Task outcome
-            assert isinstance(outcome, TaskOutcome)
-            assert "timeout" in str(outcome.error)
-
-        return rec.spans
-
-    def validate_spans(spans: Spans):
-        assert len(spans.filter(["name"], "task-dependency")) == 0
-
-        top_task_span: SpanDict = one(spans.filter(["name"], "execute-task"))
-        assert read_key(top_task_span, ["attributes", "task.id"]) == "stuck_function"
-        assert read_key(top_task_span, ["attributes", "task.task_type"]) == "Python"
-
-        # --- check timeout-guard span ---
-        timeout_span: SpanDict = one(spans.filter(["name"], "timeout-guard"))
-        assert read_key(timeout_span, ["attributes", "task.timeout_s"]) == 1.0
-
-        assert timeout_span["status"] == {
-            "description": "Timeout",
-            "status_code": "ERROR",
-        }
-
-        # --- check call-python-function span, this should exist but is not logged ---
-        assert len(spans.filter(["name"], "call-python-function")) == 0
-
-        # check nesting of above spans
-        assert spans.contains_path(
-            top_task_span,
-            timeout_span,
-        )
-
-        # assert_compatibility(spans)
-
-    validate_spans(get_test_spans())
-
-
 def test__python_function_task__run_in_parallel():
     def get_test_spans():
         with SpanRecorder() as rec:
