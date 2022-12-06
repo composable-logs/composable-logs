3b40450cd355f74a428162d37072d188a62f5ec5
Matias Dahl <matias.dahl@iki.fi>
2022-12-06 15:46:35 +0200

    first commit; new directory for python task tests

--- workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_pythonfunction_task.py
+++ workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_pythonfunction_task.py
@@ -35,59 +35,6 @@ from pynb_dag_runner.opentelemetry_helpers import (
 )
 
 
-def assert_compatibility(spans: Spans, task_id_dependencies):
-    """
-    Test:
-     - generic invariances for span timings (Steps 1, 2)
-     - order constraints in dependency DAG are satisfied by output timings (Step 3)
-
-    *** TODO: to be rewritten based on Opentelemetry spans ****
-    """
-
-    # Step 1: all task-id:s in order dependencies must have at least one runlog
-    # entry. (The converse need not hold.)
-    top_spans: Spans = spans.filter(["name"], "invoke-task")
-    task_ids_in_spans: List[str] = [span["attributes"]["task_id"] for span in top_spans]
-    # each top span should have unique span_id
-    assert len(set(task_ids_in_spans)) == len(task_ids_in_spans)
-
-    task_ids_in_dependencies: List[str] = flatten(
-        [[d["from"], d["to"]] for d in task_id_dependencies]
-    )
-    assert set(task_ids_in_dependencies) <= set(task_ids_in_spans)
-
-    # Step 2: A task retry should not start before previous attempt for running task
-    # has finished.
-    for top_span in top_spans:
-        task_id = top_span["attributes"]["task_id"]
-        run_spans = list(
-            spans.bound_under(top_span)
-            .filter(["name"], "task-run")
-            .sort_by_start_time()
-        )
-        assert len(run_spans) >= 1
-
-        for retry_nr, run_span in enumerate(run_spans):
-            assert run_span["attributes"]["task_id"] == task_id
-            assert run_span["attributes"]["retry.nr"] == retry_nr
-
-            if run_span["attributes"]["retry.max_retries"] > len(run_spans):
-                assert list(run_spans)[-1]["status"] == {"status_code": "OK"}
-
-        for s1, s2 in pairs(run_spans):
-            assert get_duration_range_us(s1).stop < get_duration_range_us(s2).start
-
-    # Step 3: Span durations should satisfy the same order constraints as in DAG
-    # run order dependencies.
-    for rule in task_id_dependencies:
-        spans_from = spans.filter(["attributes", "task_id"], rule["from"])
-        spans_to = spans.filter(["attributes", "task_id"], rule["to"])
-
-        ts0 = max([get_duration_range_us(s).stop for s in spans_from])
-        ts1 = min([get_duration_range_us(s).start for s in spans_to])
-        assert ts0 < ts1
-
-
 ### ---- Test Python task evaluation ----
 
 
