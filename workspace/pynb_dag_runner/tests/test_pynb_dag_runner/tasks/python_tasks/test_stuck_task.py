5a531acca061db46601059d018511f5caf1646ef
Matias Dahl <matias.dahl@iki.fi>
2022-12-10 21:20:17 +0200

    move stuck task tests to new span iterator

--- workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_stuck_task.py
+++ workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_stuck_task.py
@@ -14,10 +14,12 @@ from pynb_dag_runner.core.dag_runner import (
     task_from_python_function,
 )
 from pynb_dag_runner.opentelemetry_helpers import (
-    read_key,
     Spans,
     SpanDict,
     SpanRecorder,
+)
+from pynb_dag_runner.opentelemetry_task_span_parser import (
+    get_pipeline_task_artifact_iterators,
 )
 
 
@@ -30,36 +32,29 @@ def spans() -> Spans:
             time.sleep(1e6)
 
         task: RemoteTaskP = task_from_python_function(
-            f, attributes={"task.id": "stuck_function"}, timeout_s=1.0
+            f, attributes={"task.id": "stuck-function"}, timeout_s=1.0
         )
         [outcome] = start_and_await_tasks(
             [task], [task], timeout_s=10, arg="dummy value"
         )
 
-        # check Task outcome
         assert isinstance(outcome, TaskOutcome)
         assert "timeout" in str(outcome.error)
+
     return rec.spans
 
 
-def test__python_task__otel_logs_for_stuck_task(spans: Spans):
-    assert len(spans.filter(["name"], "task-dependency")) == 0
+def test__python_task__stuck_tasks__parse_spans(spans: Spans):
+    _, task_run_it = get_pipeline_task_artifact_iterators(spans)
 
-    top_task_span: SpanDict = one(spans.filter(["name"], "execute-task"))
-    assert read_key(top_task_span, ["attributes", "task.id"]) == "stuck_function"
-    assert read_key(top_task_span, ["attributes", "task.task_type"]) == "Python"
+    for task_run_summary, artefacts in [one(task_run_it)]:  # type: ignore
+        assert len(artefacts) == 0
 
-    # --- check timeout-guard span ---
-    timeout_span: SpanDict = one(spans.filter(["name"], "timeout-guard"))
-    assert read_key(timeout_span, ["attributes", "task.timeout_s"]) == 1.0
+        assert task_run_summary.attributes["task.id"] == "stuck-function"
+        assert task_run_summary.attributes["task.timeout_s"] == 1.0
+        assert task_run_summary.status["status_code"] == "ERROR"
 
-    assert timeout_span["status"] == {
-        "description": "Timeout",
-        "status_code": "ERROR",
-    }
-
-    # --- check call-python-function span, this should exist but is not logged ---
-    assert len(spans.filter(["name"], "call-python-function")) == 0
-
-    # check nesting of above spans
-    assert spans.contains_path(top_task_span, timeout_span)
+        # -
+        exceptions = task_run_summary.status["exceptions"]
+        assert len(exceptions) == 1
+        assert "timeout" in str(exceptions).lower()
