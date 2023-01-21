8eb75dee7e841715677aa9a1562915fc211e52b3
Matias Dahl <matias.dahl@iki.fi>
2023-01-21 14:07:43 +0200

    Implement new interface for executing Jupytext/Jupyter notebooks (#207)
    * first commit
    
    * fix mypy errors; continue implementation + tests for new nb interfaces
    
    * ipynb notebook eval + first test
    
    * revise error handling for notebook execution; tests
    
    * revise comments
    
    * revert initial approach
    
    * html conversion using in-memory file stream

--- workspace/pynb_dag_runner/pynb_dag_runner/notebooks_helpers.py
+++ workspace/pynb_dag_runner/pynb_dag_runner/notebooks_helpers.py
@@ -1,11 +1,13 @@
+import io, tempfile, os
+from typing import Any, Dict, Optional, Tuple, List, Union, Sequence
 from pathlib import Path
-import tempfile, os
-from typing import Any, Dict, Optional
 
-#
-import jupytext, papermill
+# -
+import pydantic as p
+import jupytext, nbformat, papermill
 from nbconvert import HTMLExporter
 
+# -
 
 """
 On the command line (bash), a Jupytext notebooks can be evaluated and converted into an
@@ -28,14 +30,131 @@ def convert_ipynb_to_html(ipynb_notebook_content: str) -> str:
 
     No evaluation of code cells.
     """
+    output, _ = HTMLExporter(template_name="classic").from_file(
+        file_stream=io.StringIO(ipynb_notebook_content)
+    )
 
-    tmp_path: str = tempfile.mkdtemp(prefix="pynb-dag-runner-temp")
-    tmp_filepath: Path = Path(tmp_path) / "temp-notebook.ipynb"
-    tmp_filepath.write_text(ipynb_notebook_content)
-    output, _ = HTMLExporter(template_name="classic").from_filename(str(tmp_filepath))
-
-    os.remove(tmp_filepath)
     return output
+
+
+class JupyterIpynbNotebookContent(p.BaseModel):
+    """
+    Model a named Jupyter notebook in .ipynb format.
+
+    Cells may or may not have been evaluated.
+    """
+
+    filepath: Path
+
+    content: str
+
+    def to_html(self) -> str:
+        return convert_ipynb_to_html(self.content)
+
+    def evaluate(
+        self,
+        tmp_path: Path,
+        cwd: Optional[Path] = None,
+        parameters: Dict[str, Any] = {},
+    ) -> Tuple[Optional[Exception], "JupyterIpynbNotebookContent"]:
+        """
+        Evaluate notebook using Papermill with the provided parameters injected.
+
+        If an exception is thrown from a cell in the notebook, execution is stopped
+        and we return the tuple: (Exception, the partially evaluated notebook)
+
+        If notebook runs succussfully we return the tuple: (None, the evaluated
+        notebook)
+        """
+
+        # Determine a temp file for input/output
+        fp, tmp_filepath = tempfile.mkstemp(
+            dir=tmp_path,
+            prefix="temp-notebook-",
+            suffix=".ipynb",
+        )
+        assert tmp_path == Path(tmp_filepath).parent
+        os.write(fp, self.content.encode(encoding="utf-8"))
+        os.close(fp)
+
+        def get_notebook():
+            return JupyterIpynbNotebookContent(
+                filepath=self.filepath,
+                content=Path(tmp_filepath).read_text(),
+            )
+
+        try:
+            # For all parameters, see
+            # https://github.com/nteract/papermill/blob/main/papermill/cli.py
+            # https://github.com/nteract/papermill/blob/main/papermill/execute.py
+            papermill.execute_notebook(
+                input_path=tmp_filepath,
+                output_path=tmp_filepath,
+                parameters=parameters,
+                request_save_on_cell_execute=True,
+                kernel_name="python",
+                language="python",
+                progress_bar=True,
+                stdout_file=None,
+                stderr_file=None,
+                log_output=True,
+                cwd=cwd if cwd is not None else tmp_path,
+            )
+            return None, get_notebook()
+
+        except BaseException as e:
+            # Convert any Papermill custom exceptions into a standard Python
+            # Exception-class, see
+            #
+            # https://github.com/nteract/papermill/blob/main/papermill/exceptions.py
+            #
+            # Otherwise, we get problems with Ray not able to serialize/deserialize
+            # the Exception
+            return Exception(str(e)), get_notebook()
+
+        finally:
+            # This may not run if the Python process is cancelled by Ray (eg timeout).
+            os.remove(tmp_filepath)
+
+
+class JupytextNotebookContent(p.BaseModel):
+    """
+    Contain content of Jupyter notebook in Jupytext (non-JSON) file format.
+
+    This file format contains the input code cells, but no output.
+    """
+
+    # eg "notebooks/eda.py" or "eda.py"; should end with .py
+    filepath: Path
+
+    content: str
+
+    def __str__(self):
+        return "\n".join(
+            [
+                f"JupytextNotebookContent (relative_filepath={self.filepath})",
+                80 * "=",
+                (self.content)[:1000],
+                "...",
+                80 * "=",
+            ]
+        )
+
+    def to_ipynb(self) -> JupyterIpynbNotebookContent:
+        # Convert Jupytext input into ipynb file (without executing cells)
+        nb = jupytext.reads(self.content, fmt="py:percent")
+        ipynb_content: str = jupytext.writes(
+            nb,
+            version=nbformat.NO_CONVERT,
+            fmt="notebook",
+        )
+        return JupyterIpynbNotebookContent(
+            filepath=self.filepath.with_suffix(".ipynb"),
+            content=ipynb_content,
+        )
+
+
+# --- old interface, to be deleted ---
 
 
 class JupyterIpynbNotebook:
--- workspace/pynb_dag_runner/tests/test_pynb_dag_runner/test_notebook_helpers.py
+++ workspace/pynb_dag_runner/tests/test_pynb_dag_runner/test_notebook_helpers.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os
+import os, json
 
 #
 import pytest
@@ -9,11 +9,13 @@ from pynb_dag_runner.notebooks_helpers import (
     JupytextNotebook,
     JupyterIpynbNotebook,
     convert_ipynb_to_html,
+    JupytextNotebookContent,
+    JupyterIpynbNotebookContent,
 )
 from pynb_dag_runner.helpers import read_json
 
-# Example content of a Jupyter notebook stored in the py-percent file format.
-TEST_JUPYTEXT_NOTEBOOK = """# %%
+# Test Jupyter notebooks stored in the Jupytext py-percent file format.
+TEST_JUPYTEXT_OK_NOTEBOOK = """# %%
 # %% tags=["parameters"]
 # %%
 # Example comment
@@ -23,17 +25,27 @@ print(f"variable_a={variable_a}")
 # %%
 """
 
+TEST_JUPYTEXT_FAIL_NOTEBOOK = """# %%
+# %% tags=["parameters"]
+# %%
+# Example comment
+print(1234 + 234 + 54 + 6)
+# %%
+raise Exception("Failed notebook123")
+# %%
+"""
 
-def write_test_jupytext_notebook(path: Path) -> JupytextNotebook:
+
+def write_test_jupytext_OK_notebook(path: Path) -> JupytextNotebook:
     output_path = path / "notebook.py"
-    output_path.write_text(TEST_JUPYTEXT_NOTEBOOK)
+    output_path.write_text(TEST_JUPYTEXT_OK_NOTEBOOK)
 
-    assert os.path.getsize(output_path) == len(TEST_JUPYTEXT_NOTEBOOK)
+    assert os.path.getsize(output_path) == len(TEST_JUPYTEXT_OK_NOTEBOOK)
     return JupytextNotebook(output_path)
 
 
-def test_can_convert_jupytext_notebook_to_ipynb_and_html(tmp_path: Path):
-    notebook_py: JupytextNotebook = write_test_jupytext_notebook(tmp_path)
+def test_nb_can_convert_jupytext_notebook_to_ipynb_and_html(tmp_path: Path):
+    notebook_py: JupytextNotebook = write_test_jupytext_OK_notebook(tmp_path)
 
     # Convert py-percent jupytext file into ipynb-notebook file format
     notebook_ipynb = notebook_py.to_ipynb()
@@ -48,8 +60,8 @@ def test_can_convert_jupytext_notebook_to_ipynb_and_html(tmp_path: Path):
     assert "# Example comment" in filepath_html.read_text()
 
 
-def test_can_convert_jupytext_notebook_to_ipynb_and_evaluate(tmp_path: Path):
-    notebook_py: JupytextNotebook = write_test_jupytext_notebook(tmp_path)
+def test_nb_can_convert_jupytext_notebook_to_ipynb_and_evaluate(tmp_path: Path):
+    notebook_py: JupytextNotebook = write_test_jupytext_OK_notebook(tmp_path)
 
     # Convert py-percent jupytext file into ipynb-notebook file format
     notebook_ipynb = notebook_py.to_ipynb()
@@ -64,18 +76,18 @@ def test_can_convert_jupytext_notebook_to_ipynb_and_evaluate(tmp_path: Path):
     assert "variable_a=hello" in notebook_eval_ipynb.filepath.read_text()
 
 
-def test_random_ipynb_notebook_path(tmp_path: Path):
+def test_nb_random_ipynb_notebook_path(tmp_path: Path):
     notebook_ipynb = JupyterIpynbNotebook.temp(tmp_path)
 
     assert not notebook_ipynb.filepath.is_file()
     assert str(notebook_ipynb.filepath).startswith(str(tmp_path))
 
 
-def test_evaluate_jupytext_notebook(tmp_path: Path):
+def test_nb_evaluate_jupytext_notebook(tmp_path: Path):
     output_path = tmp_path / "output"
     output_path.mkdir()
 
-    notebook_py: JupytextNotebook = write_test_jupytext_notebook(tmp_path)
+    notebook_py: JupytextNotebook = write_test_jupytext_OK_notebook(tmp_path)
 
     output_ipynb = JupyterIpynbNotebook(output_path / "foo.ipynb")
 
@@ -89,8 +101,8 @@ def test_evaluate_jupytext_notebook(tmp_path: Path):
     assert "variable_a=baz" in output_ipynb.filepath.read_text()
 
 
-def test_convert_ipynb_string_to_html(tmp_path: Path):
-    notebook_py: JupytextNotebook = write_test_jupytext_notebook(tmp_path)
+def test_nb_convert_ipynb_string_to_html(tmp_path: Path):
+    notebook_py: JupytextNotebook = write_test_jupytext_OK_notebook(tmp_path)
 
     output_ipynb = JupyterIpynbNotebook(tmp_path / "foo.ipynb")
 
@@ -108,23 +120,13 @@ def test_convert_ipynb_string_to_html(tmp_path: Path):
 @pytest.fixture
 def failing_jupytext_notebook(tmp_path: Path) -> JupytextNotebook:
     output_path: Path = tmp_path / "failing_notebook.py"
+    output_path.write_text(TEST_JUPYTEXT_FAIL_NOTEBOOK)
 
-    fail_jupytext_notebook = """# %%
-# %% tags=["parameters"]
-# %%
-# Example comment
-print(1234 + 234 + 54 + 6)
-# %%
-raise Exception("Failed notebook123")
-# %%
-"""
-    output_path.write_text(fail_jupytext_notebook)
-
-    assert len(output_path.read_text()) == len(fail_jupytext_notebook)
+    assert len(output_path.read_text()) == len(TEST_JUPYTEXT_FAIL_NOTEBOOK)
     return JupytextNotebook(output_path)
 
 
-def test_evaluate_jupytext_notebook_that_fails(
+def test_nb_evaluate_jupytext_notebook_that_fails(
     failing_jupytext_notebook: JupytextNotebook,
 ):
 
@@ -144,3 +146,68 @@ def test_evaluate_jupytext_notebook_that_fails(
         assert output_ipynb.filepath.is_file()
         for s in ["variable_xyz", "foobarbaz", str(1234 + 234 + 54 + 6)]:
             assert s in output_ipynb.filepath.read_text()
+
+
+# --- New notebook interface ---
+
+
+@pytest.fixture
+def ok_jupytext_notebook() -> JupytextNotebookContent:
+    return JupytextNotebookContent(
+        filepath=Path("nb/ok_notebook.py"),
+        content=TEST_JUPYTEXT_OK_NOTEBOOK,
+    )
+
+
+def test_nnb_convert_jupytext_content_to_ipynb_and_html_content(
+    ok_jupytext_notebook: JupytextNotebookContent,
+):
+    ipynb_nb = ok_jupytext_notebook.to_ipynb()
+
+    # check jupytext -> ipynb conversion output
+    assert ipynb_nb.filepath == Path("nb/ok_notebook.ipynb")
+    json.loads(ipynb_nb.content)
+    assert "print(1 + 12 + 123)" in ipynb_nb.content
+    assert len(ipynb_nb.content) > len(ok_jupytext_notebook.content)
+
+    # check ipynb -> html conversion output
+    nb_html: str = ipynb_nb.to_html()
+    assert len(nb_html) > len(ok_jupytext_notebook.content)
+    assert "<html>" in nb_html
+
+
+def test_nnb_evaluate_jupytext_content_notebook(
+    ok_jupytext_notebook: JupytextNotebookContent,
+    tmp_path: Path,
+):
+    parameters = {"variable_a": "aaaaabbbbbbccccccc"}
+
+    err, evaluated_ipynb_nb = ok_jupytext_notebook.to_ipynb().evaluate(
+        tmp_path, parameters=parameters
+    )
+    assert err is None
+
+    # check jupytext -> ipynb conversion output
+    assert evaluated_ipynb_nb.filepath == Path("nb/ok_notebook.ipynb")
+    json.loads(evaluated_ipynb_nb.content)
+
+    assert "print(1 + 12 + 123)" in evaluated_ipynb_nb.content
+    assert parameters["variable_a"] in evaluated_ipynb_nb.content
+    assert str(1 + 12 + 123) in evaluated_ipynb_nb.content
+
+
+def test_nnb_evaluate_failing_jupytext_notebook(tmp_path: Path):
+    err, partially_evaluated_notebook = (
+        JupytextNotebookContent(
+            filepath=Path("nb/fail_notebook.py"),
+            content=TEST_JUPYTEXT_FAIL_NOTEBOOK,
+        )
+        .to_ipynb()
+        .evaluate(tmp_path, parameters={"variable_xyz": "foobarbaz"})
+    )
+
+    assert "Failed notebook123" in str(err)
+
+    assert partially_evaluated_notebook.filepath == Path("nb/fail_notebook.ipynb")
+    for s in ["variable_xyz", "foobarbaz", str(1234 + 234 + 54 + 6)]:
+        assert s in partially_evaluated_notebook.content
