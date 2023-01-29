e2ad8cb674053cbfb30d31b6312d5607860e09c6
Matias Dahl <matias.dahl@iki.fi>
2023-01-29 11:07:18 +0200

    delete old tests from test_ok_or_failed_task.py

--- workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_ok_or_failed_task.py
+++ workspace/pynb_dag_runner/tests/test_pynb_dag_runner/tasks/python_tasks/test_ok_or_failed_task.py
@@ -1,5 +1,3 @@
-from functools import lru_cache
-
 # -
 import ray
 import pytest
@@ -7,121 +5,16 @@ import pytest
 # -
 from pynb_dag_runner.opentelemetry_helpers import Spans
 from pynb_dag_runner.helpers import one, Failure
-from pynb_dag_runner.core.dag_runner import (
-    TaskOutcome,
-    start_and_await_tasks,
-    RemoteTaskP,
-    task_from_python_function,
-)
 from pynb_dag_runner.opentelemetry_task_span_parser import parse_spans
 
 from pynb_dag_runner.opentelemetry_helpers import (
-    read_key,
     Spans,
-    SpanDict,
     SpanRecorder,
 )
 from pynb_dag_runner.wrappers import task, run_dag, ExceptionGroup
 
 # Error message for failing tasks
 ERROR_MSG = "!!!Exception-12342!!!"
-
-
-@lru_cache
-def get_spans(task_should_fail: bool) -> Spans:
-    """
-    Get test spans for Python task that fails according to task_should_fail.
-
-    Note:
-    pytest can parameterise fixtures, but it seems complicated to also keep
-    track of both input parameter and evaluated fixture. Therefore, we get
-    spans manually in each test.
-
-    https://docs.pytest.org/en/stable/example/parametrize.html#indirect-parametrization
-
-    """
-    assert False
-    with SpanRecorder() as rec:
-
-        def f(_):
-            if task_should_fail:
-                raise Exception(ERROR_MSG)
-            else:
-                return 123
-
-        task: RemoteTaskP = task_from_python_function(
-            f, attributes={"workflow.foo": "bar", "task.foo": "my_test_func"}
-        )
-        [outcome] = start_and_await_tasks(
-            [task], [task], timeout_s=10, arg="dummy value"
-        )
-
-        # check Task outcome
-        assert isinstance(outcome, TaskOutcome)
-        if task_should_fail:
-            assert ERROR_MSG in str(outcome.error)
-        else:
-            assert outcome.return_value == 123
-
-    return rec.spans
-
-
-@pytest.mark.skipif(True, reason="remove after move to new Ray interface")
-@pytest.mark.parametrize("task_should_fail", [True, False])
-def test__python_task__ok_or_fail__parsed_spans(task_should_fail: bool):
-    spans = get_spans(task_should_fail)  # manually get spans for parameter
-
-    pipeline_summary = parse_spans(spans)
-
-    assert pipeline_summary.task_dependencies == set()
-    assert pipeline_summary.attributes == {"workflow.foo": "bar"}
-
-    for task_summary in [one(pipeline_summary.task_runs)]:  # type: ignore
-        assert len(task_summary.logged_values) == 0
-        assert len(task_summary.logged_artifacts) == 0
-
-        assert task_summary.is_success() == (not task_should_fail)
-        assert task_summary.attributes == {
-            "workflow.foo": "bar",
-            "task.foo": "my_test_func",
-            "task.num_cpus": 1,
-            "task.task_type": "Python",
-        }
-
-        if task_should_fail:
-            # now two exceptions: same exception is raised in function and in runner
-            assert len(task_summary.exceptions) == 2
-
-            for e in task_summary.exceptions:
-                assert e["attributes"]["exception.message"] == ERROR_MSG
-
-
-@pytest.mark.skipif(True, reason="move away from this file")
-def test__python_task__ok_or_fail__validate_spans():
-    spans = get_spans(task_should_fail=False)
-
-    assert len(spans.filter(["name"], "task-dependency")) == 0
-
-    top_task_span: SpanDict = one(spans.filter(["name"], "execute-task"))
-    assert read_key(top_task_span, ["attributes", "task.foo"]) == "my_test_func"
-    assert read_key(top_task_span, ["attributes", "task.task_type"]) == "Python"
-
-    assert len(spans.bound_inclusive(top_task_span).exception_events()) == 0
-
-    # --- check timeout-guard span ---
-    timeout_span: SpanDict = one(spans.filter(["name"], "timeout-guard"))
-    assert timeout_span["status"] == {"status_code": "OK"}  # no timeouts
-
-    # --- check call-python-function span ---
-    call_function_span: SpanDict = one(spans.filter(["name"], "call-python-function"))
-    assert call_function_span["status"] == {"status_code": "OK"}
-
-    # check nesting of above spans
-    assert spans.contains_path(
-        top_task_span,
-        timeout_span,
-        call_function_span,
-    )
 
 
 # --- assert that tasks are not retried by Ray ---
